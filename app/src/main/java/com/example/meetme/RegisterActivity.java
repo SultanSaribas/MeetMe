d6136634b5c0e941e2bb49627d143f9d34e0ecf4
SultanSaribas <saribassultan@gmail.com>
2020-12-23 23:54:37 +0300

    Navigation between Signin and Register

--- app/src/main/java/com/example/meetme/RegisterActivity.java
+++ app/src/main/java/com/example/meetme/RegisterActivity.java
@@ -2,13 +2,35 @@ package com.example.meetme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button btn_register_with_email_activity_register;
+    EditText et_namesurname_register, et_password_register, et_email_register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btn_register_with_email_activity_register=findViewById(R.id.btn_register_with_email_activity_register);
+        et_namesurname_register=findViewById(R.id.et_namesurname_register);
+        et_password_register=findViewById(R.id.et_password_register);
+        et_email_register=findViewById(R.id.et_email_register);
+
+        btn_register_with_email_activity_register.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/meetme/SignInActivity.java
+++ app/src/main/java/com/example/meetme/SignInActivity.java
@@ -2,7 +2,9 @@ package com.example.meetme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,5 +17,31 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btn_login=findViewById(R.id.btn_login);
+        btn_register_inlogin=findViewById(R.id.btn_register_inlogin);
+        et_email_login=findViewById(R.id.et_email_login);
+        et_password_login=findViewById(R.id.et_password_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_register_inlogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SignInActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
