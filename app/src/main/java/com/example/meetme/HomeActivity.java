e77ab6cfa6dbdcb4dfd999031c5daf079131b9c9
SultanSaribas <saribassultan@gmail.com>
2020-12-24 00:57:47 +0300

    Home Activity Added

--- app/src/main/java/com/example/meetme/HomeActivity.java
+++ app/src/main/java/com/example/meetme/HomeActivity.java
@@ -3,12 +3,27 @@ package com.example.meetme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button science_and_technology,sport_button,art_and_culture,entertainment,education,history,med,game, business,psychology;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        science_and_technology=findViewById(R.id.science_and_technology_button);
+        sport_button=findViewById(R.id.sport_button);
+        art_and_culture=findViewById(R.id.art_and_culture_button);
+        entertainment=findViewById(R.id.entertainment_button);
+        education=findViewById(R.id.education_button);
+        history=findViewById(R.id.history_button);
+        med=findViewById(R.id.med_button);
+        game=findViewById(R.id.game_button);
+        business=findViewById(R.id.business_button);
+        psychology=findViewById(R.id.psychology_button);
+
     }
 }
