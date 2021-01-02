f77952ca03798e6a4efb072c8be3b04516094fd7
SultanSaribas <saribassultan@gmail.com>
2021-01-02 23:13:51 +0300

    navigated navigationbar in all pages

--- app/src/main/java/com/example/meetme/EventDetailActivity.java
+++ app/src/main/java/com/example/meetme/EventDetailActivity.java
@@ -1,8 +1,14 @@
 package com.example.meetme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EventDetailActivity extends AppCompatActivity {
 
@@ -10,5 +16,35 @@ public class EventDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
+
+        navigationbar();
+    }
+
+
+    public void navigationbar() {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation_bar);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        Intent intent = new Intent(EventDetailActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_search:
+                        intent = new Intent(EventDetailActivity.this, DiscoverActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_profile:
+                        intent = new Intent(EventDetailActivity.this, ProfileActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/meetme/ProfileActivity.java
+++ app/src/main/java/com/example/meetme/ProfileActivity.java
@@ -1,8 +1,14 @@
 package com.example.meetme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +16,34 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        navigationbar();
+    }
+
+    public void navigationbar() {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation_bar);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_search:
+                        intent = new Intent(ProfileActivity.this, DiscoverActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_profile:
+                        intent = new Intent(ProfileActivity.this, ProfileActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
