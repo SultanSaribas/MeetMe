243e2f4751f8bf82aa87e014a685f76c19d19a0c
SultanSaribas <saribassultan@gmail.com>
2021-01-02 22:55:33 +0300

    app logo and navigationbar

--- app/src/main/java/com/example/meetme/HomeActivity.java
+++ app/src/main/java/com/example/meetme/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.example.meetme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -45,5 +50,33 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+            navigationbar();
+
     }
+
+   public void navigationbar(){
+       BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation_bar);
+       Menu menu = bottomNavigationView.getMenu();
+       MenuItem menuItem = menu.getItem(0);
+       menuItem.setChecked(true);
+       bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               switch (item.getItemId()){
+                   case R.id.nav_home:
+                       break;
+                   case R.id.nav_search:
+                       Intent intent = new Intent(HomeActivity.this, DiscoverActivity.class);
+                       startActivity(intent);
+                       break;
+                   case R.id.nav_profile:
+                       intent = new Intent(HomeActivity.this, ProfileActivity.class);
+                       startActivity(intent);
+                       break;
+               }
+               return false;
+           }
+       });
+   }
+
 }
